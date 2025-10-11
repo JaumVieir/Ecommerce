bbdddff87288532ae4c8fe6b0fa13f9eafa4c84d
JaumVieir <vitor_silva36@hotmail.com>
2025-10-11 14:02:14 -0300

    Iague

--- Backend/BD/APIBD/produtos.js
+++ Backend/BD/APIBD/produtos.js
@@ -111,7 +111,7 @@ router.get("/predicao/:id", async (req, res) => {
     const input = JSON.stringify({ id });
 
     const arquivo = spawn("python", [
-      "C:/Users/User/Projetos/Ecommerce/Backend/BD/Recomendacao/Recomendacao.py",
+      "C:/Users/Bitlab/Desktop/Ecommerce/Backend/BD/Recomendacao/Recomendacao.py",
     ]);
 
     let respostas = "";
