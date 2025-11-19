14247cec0921cd49bd321bb971a2b67e7c6cb9bf
Jaum <vitor36silva@gmail.com>
2025-11-19 01:50:26 -0300

    logo 5

--- Frontend/ecommerce/src/views/TodosProdutos.vue
+++ Frontend/ecommerce/src/views/TodosProdutos.vue
@@ -45,6 +45,7 @@ export default {
 },
 
   async mounted() {
+    this.paginaAtual = 1;
     // Inicializa a página a partir de uma propriedade global (se existir) ou sessionStorage
     const globalPage = window.todosProdutosPagina;
     const saved = sessionStorage.getItem('todosProdutos_paginaAtual');
